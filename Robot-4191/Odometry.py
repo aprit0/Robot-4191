a1dc1b06318a50c1df700a46b84806041706fbba
Gemma <gbie0001@student.monash.edu>
2022-08-14 20:23:56 +1000

    fixed the find_position function and tested it - x outputs seem a little suss (too large) but we ignore for now

--- Robot-4191/Odometry.py
+++ Robot-4191/Odometry.py
@@ -10,7 +10,7 @@ import numpy as np
 # encoder steps and velocity are dependent on each other sooooooo
 class Odometry:
 
-    def __init__(self, velocity, steer_angle, left_encoder_steps, right_encoder_steps, delta_time): #delete left and right encoder steps for ROS implementation
+    def __init__(self, velocity, steer_angle, delta_time):
         """
         Inputs: velocity or angle (not both)
         The code will use these inputs to tell the motors to move so that the velocity or angle change are achieved
@@ -21,8 +21,6 @@ class Odometry:
         #can use the below values within any function in the class
         self.velocity = velocity
         self.steer = steer_angle
-        self.left = left_encoder_steps
-        self.right = right_encoder_steps
         self.time = delta_time
         # super().__init__('encoder')
         # self.publisher_ = self.create_publisher(Float32, 'encoder_rpm', 10)
@@ -31,36 +29,30 @@ class Odometry:
         # self.timer = self.create_timer(timer_period, self.timer_callback)
         # self.t_0 = time.time()
 
-    def find_position(self, x_old, y_old, theta_old, step_size):
-        #take the previous position as an input as well as time elapsed
-        if self.velocity != 0:
-            dist = step_size*self.right #step_size*encoder steps taken
-            theta = theta_old #theta_old remains the same as there is no rotation
-            dtheta = 0 #no change in angle
-            x = x_old + dist*np.cos(theta)
-            y = y_old + dist*np.sin(theta)
-            dx = (x - x_old)/self.time #change in distance over time
-            dy = (y - y_old)/self.time
+    def find_position(self, x_old, y_old, theta_old, step_size, left_encoder_steps, right_encoder_steps, length):
 
-        else: #self.steer !=0
-            x = x_old
-            y = y_old
-            dx = 0 #no change in location
-            dy = 0
-            theta = theta_old + self.steer #assuming the steer angle is wrt the previous angle
-            dtheta = (theta - theta_old)/self.time
+        #assuming encoder steps are negative for moving backwards
+        dist_left = step_size*left_encoder_steps #step_size*encoder steps taken
+        dist_right = step_size*right_encoder_steps
 
-        print(f"x = {x:2f}, y = {y:2f}, theta = {theta:2f}, dx = {dx:2f}, dy = {dy:2f}, dtheta = {dtheta:2f}")
+        x = x_old + ((dist_left + dist_right) / 2) * np.cos(theta_old)
+        y = y_old + ((dist_left + dist_right) / 2) * np.sin(theta_old)
+        theta = theta_old + (dist_right - dist_left) / length
+        dx = (x - x_old) / self.time
+        dy = (y - y_old) / self.time
+        dtheta = (theta - theta_old) / self.time
+
+        print(f"x = {x:6f}, y = {y:6f}, theta = {theta:6f}, dx = {dx:6f}, dy = {dy:6f}, dtheta = {dtheta:6f}")
 
         return x, y, theta, dx, dy, dtheta
 
     def travel(self):
-        pass#inputs =
-        #ask the robot to move
+        pass
+        #ask the robot to move in the direction and speed given by velocity
 
     def change_angle(self):
         pass#inputs =
-        #ask the robot to rotate
+        #ask the robot to rotate in the direction given by steer angle
 
 
     # def timer_callback(self):
@@ -91,15 +83,18 @@ class Odometry:
 if __name__ == '__main__':
     #main()
     #stuff to test the code
+
     velocity = 0.1 # between 0 and 1 (1 = max velocity of motor)
     steer_angle = 0 # rad
     delta_time = 3 # seconds
-    left_encoder_steps = 50 #dependent on velocity need to deal with thissssssssssssssss
-    right_encoder_steps = 50 #dependent on velocity need to deal with thissssssssssssssss
-    test = Odometry(velocity, steer_angle, left_encoder_steps, right_encoder_steps, delta_time)
+    left_encoder_steps = 51
+    right_encoder_steps = 50
+    test = Odometry(velocity, steer_angle, delta_time)
     wheel_radius = 3
+    length = 5 #distance between wheels
     step_size = wheel_radius * ((2 * np.pi) / 48) #48 encoder steps for the wheel
     x_old = 0
     y_old = 0
     theta_old = np.pi/2 #the first theta must be pi/2 so that y is in the forward direction.
-    test.find_position(x_old, y_old, theta_old, step_size)
+    for i in range(10):
+        x_old, y_old, theta_old, dx, dy, dtheta = test.find_position(x_old, y_old, theta_old, step_size, left_encoder_steps, right_encoder_steps, length)
