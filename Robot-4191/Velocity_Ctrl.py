8ccc03521c8c31c16669108a8e470583ff93a6ca
Gemma <gbie0001@student.monash.edu>
2022-08-19 21:31:25 +1000

    some code for testing velocity and steer done

---
+++ Robot-4191/Velocity_Ctrl.py
@@ -0,0 +1,66 @@
+from Utils.utils import Motor
+import time
+'''
+TODO:
+-test the velocity at different speeds from 0 to 1 and enter results in excel so the actual velocity can be calculated
+-test steer angle as well so that we can convert a time to an angle
+'''
+
+
+def velocity(vel):
+
+    # convert the velocity (0-1) to an actual velocity after testing
+
+    t_0 = time.time()
+    while (time.time() - t_0 < 3): #time to drive can be an input if needed
+        motor_left.forward(vel)
+        motor_right.forward(vel)
+
+def steer_angle(steer):
+
+    # convert the steer - currently a time to an angle after testing
+
+    t_0 = time.time()
+    while (time.time() - t_0 < abs(steer)): #the speed of turning must remain constant, while the time allowed to turn changes for different angles
+        if (steer > 0): #turn left
+            motor_left.backward(1)
+            motor_right.forward(1)
+        else: #turn right
+            motor_left.forward(1)
+            motor_right.backward(1)
+
+if __name__ == "__main__":
+    main()
+
+    motor_right = Motor(22, 23)
+    motor_left = Motor(27, 24)
+
+    state = True
+    while (state == True):
+        #ask user if they want to travel straight or turn
+        ans0 = input("Would you like to travel straight or turn? \n1: straight\n2: turn")
+
+        #ask user to input a velocity or steer angle
+        if (ans0 == 1):
+            #ask for velocity
+            vel = input("Enter a velocity (0 to 1)")
+
+            #error check
+            while (vel > 1 or vel < 0):
+                vel = input("Enter a velocity (0 to 1)")
+
+            velocity(vel)
+
+        elif (ans0 == 2):
+            #ask for a steer angle
+            steer = input("Please enter a steer angle in degrees:")
+
+            steer_angle(steer)
+
+        #ask user if they would like to continue
+        ans1 = input("Would you like to continue? [y/n]")
+
+        if (ans1 == n or N):
+            state = False
+
+
