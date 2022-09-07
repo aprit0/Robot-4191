b43af066f65d0428a62fb3433800f040c20d69ec
Gemma Biezen <103622022+GemmaTrinity@users.noreply.github.com>
2022-09-07 13:24:06 +1000

    Update Camera.py

--- Robot-4191/Python_Test/Camera.py
+++ Robot-4191/Python_Test/Camera.py
@@ -1,13 +1,21 @@
-import cv2
-import time
 
-def capture(i):
-    cap = cv2.VideoCapture('~/Documents', cv2.CAP_V4L)
-    time.sleep(1)
-    ret,frame = cap.read()
-    cv2.imwrite('%d_%s.jpg' % i,time.time(),frame)
-    cap.release()
+#import cv2
+#import time
 
-for i in range(5):
-    capture(i)
-    print(i)
+#def capture(i):
+#    cap = cv2.VideoCapture('~/Documents', cv2.CAP_V4L)
+#    time.sleep(1)
+#    ret,frame = cap.read()
+#    cv2.imwrite('%d_%s.jpg' % i,time.time(),frame)
+ #   cap.release()
+
+#for i in range(5):
+ #   capture(i)
+  #  print(i)
+    
+    
+from picamera import PiCamera
+camera = PiCamera()
+time.sleep(2)
+camera.capture("~/Documents/pi/Pictures/img.jpg")
+print("Done.")
