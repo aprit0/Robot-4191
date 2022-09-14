7d933f53230cbf79ec20e35e385bac469995cfc6
aprit0 <a.pritchard@outlook.com>
2022-09-14 11:32:02 +1000

    added test images

---
+++ Robot-4191/Python_Test/Camera_1.py
@@ -0,0 +1,13 @@
+import cv2
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+counter = 0
+while True:
+        x = input('Take image with 0')
+        if x == '0':
+                ret, frame = cap.read()
+                cv2.imwrite('test_img/image_{}.jpg'.format(counter), frame)
+                print('Took image')
+                counter += 1
+cap.release()
