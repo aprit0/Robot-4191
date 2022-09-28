cd76f7a9c4e064dd61ed3000b97e947e84d7de70
aprit <70569942+aprit0@users.noreply.github.com>
2022-09-28 15:52:11 +1000

    Update Camera_1.py

--- Robot-4191/Python_Test/Camera_1.py
+++ Robot-4191/Python_Test/Camera_1.py
@@ -5,10 +5,9 @@ cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
 while True:
+        ret, frame = cap.read()
         x = input('Take image with 0')
         if x == '0':
-                
-                ret, frame = cap.read()
                 cv2.imwrite('test_img/image_{}_{}.jpg'.format(counter, time.time()), frame)
                 print('Took image')
                 counter += 1
