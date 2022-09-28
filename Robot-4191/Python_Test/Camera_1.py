22b4266dd2ee4dae7e09096da3d3885d3c9fa3f2
aprit <70569942+aprit0@users.noreply.github.com>
2022-09-28 15:16:19 +1000

    Update Camera_1.py

--- Robot-4191/Python_Test/Camera_1.py
+++ Robot-4191/Python_Test/Camera_1.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 counter = 0
 cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
@@ -8,7 +9,7 @@ while True:
         if x == '0':
                 
                 ret, frame = cap.read()
-                cv2.imwrite('test_img/image_{}.jpg'.format(counter), frame)
+                cv2.imwrite('test_img/image_{}_{}.jpg'.format(counter, time.time()), frame)
                 print('Took image')
                 counter += 1
 cap.release()
